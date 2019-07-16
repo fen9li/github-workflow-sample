92e8f1c02810d8bc4ce0119a000c6737b666d9d0
Sergei Sorokovykh <vinchilive@gmail.com>
2019-07-17 10:57:52 +1200

    fix(feed offer): page loading indicator

--- packages/online-offers/src/views/feed-update/offer/feed-offer.vue
+++ packages/online-offers/src/views/feed-update/offer/feed-offer.vue
@@ -33,9 +33,7 @@ export default {
     },
   },
   async created() {
-    this.fetchFeedOffer()
-  },
-  async mounted() {
+    await this.fetchFeedOffer()
     this.loading = false
   },
   methods: {
