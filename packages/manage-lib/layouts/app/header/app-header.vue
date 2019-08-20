bc14a3a1c3eb97890af98e2d6cf60fcf634bff58
Konstantin Zakablukovsky <2cynicus@gmail.com>
2019-08-20 10:55:27 +1000

    feat(app-header): removed select

--- packages/manage-lib/layouts/app/header/app-header.vue
+++ packages/manage-lib/layouts/app/header/app-header.vue
@@ -37,27 +37,6 @@ export default {
       >
       <span>{{ title }}</span>
     </router-link>
-    <el-select
-      v-if="appCode !== 'payments'"
-      :value="$auth.config.label"
-      placeholder="Select Panel"
-    >
-      <template v-for="item in applications">
-        <a
-          v-if="!item.scope || $grant(item.scope)"
-          :key="item.code"
-          :href="item.path"
-          class="app-header__link"
-        >
-          <el-option
-            :label="item.label"
-            :value="item.label"
-          >
-            {{ item.label }}
-          </el-option>
-        </a>
-      </template>
-    </el-select>
     <app-profile />
   </div>
 </template>
