5669fab52f42bf494c06cb5e2623a8e10a976b71
GenaRazmakhnin <gena.razmakhnin@gmail.com>
2019-09-02 12:13:38 +0800

    feat(dashboard): add title and subtitle

--- packages/super/src/views/dashboard/dashboard.vue
+++ packages/super/src/views/dashboard/dashboard.vue
@@ -1,9 +1,13 @@
 <script>
 import applications from '@lib/applications'
+import { mapGetters } from 'vuex'
 
 export default {
   inheritAttrs: false,
   computed: {
+    ...mapGetters('auth', [
+      'profile',
+    ]),
     applications() {
       return applications
     },
@@ -12,30 +16,38 @@ export default {
 </script>
 
 <template>
-  <main-layout title="Dashboard">
-    <template v-for="(item, name) in applications">
-      <el-col
-        v-if="(!item.scope || $grant(item.scope)) && name !== 'super'"
-        :key="item.path"
-        :class="[$style.appsItem, 'grid-content']"
-      >
-        <a :href="item.path">
-          <el-card shadow="hover">
-            <div :class="$style.card">
-              <img :src="`/images/${item.icon}`">
-              <div :class="$style.cardLabel">{{ item.label }}</div>
-              <div :class="$style.cardArrow">
-                <img src="/images/arrow-right.svg">
+  <main-layout
+    :title="`Welcome ${profile.given_name || 'Username'}`"
+    subtitle="These are shortcuts to all your platform products."
+  >
+    <div :class="$style.main">
+      <template v-for="(item, name) in applications">
+        <el-col
+          v-if="(!item.scope || $grant(item.scope)) && name !== 'super'"
+          :key="item.path"
+          :class="[$style.appsItem, 'grid-content']"
+        >
+          <a :href="item.path">
+            <el-card shadow="hover">
+              <div :class="$style.card">
+                <img :src="`/images/${item.icon}`">
+                <div :class="$style.cardLabel">{{ item.label }}</div>
+                <div :class="$style.cardArrow">
+                  <img src="/images/arrow-right.svg">
+                </div>
               </div>
-            </div>
-          </el-card>
-        </a>
-      </el-col>
-    </template>
+            </el-card>
+          </a>
+        </el-col>
+      </template>
+    </div>
   </main-layout>
 </template>
 
 <style lang="scss" module>
+.main {
+  padding: 0 38px;
+}
 
 .appsItem {
   width: 50%;
