b27a83877c10f82a88fdf20d452f40bf14a8976f
Konstantin Zakablukovsky <2cynicus@gmail.com>
2019-09-02 11:18:12 +1000

    fix(app-super): new roles & user views

--- packages/super/src/views/roles/roles.vue
+++ packages/super/src/views/roles/roles.vue
@@ -1,93 +1,13 @@
 <script>
-// fixme temporary, use default api processor in production
-import Auth0ApiProcessor from '@loyalty-corp/manage-lib/processors/temp/auth0-api-processor'
-import tableConfig from './roles-table'
-
-const { VUE_APP_AUTH0_DOMAIN } = process.env
-
 export default {
-  name: 'UserInfo',
-  page: {
-    title: 'User Information',
-  },
-  components: {
-  },
-  props: {
-    id: {
-      type: String,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      resourceServers: [],
-      processor: new Auth0ApiProcessor({
-        disableQueryString: true,
-        query: {
-          hide: [
-            'id',
-          ],
-        },
-        component: this,
-        path:
-          (() => {
-            const { $route: { name } = {} } = this
-            if ( name === 'user-roles') {
-              return `https://${VUE_APP_AUTH0_DOMAIN}/api/v2/users/${this.id}/roles`
-            } else {
-              return `https://${VUE_APP_AUTH0_DOMAIN}/api/v2/roles`
-            }
-          })(),
-      }),
-      filters: tableConfig.filters,
-      columns: tableConfig.columns,
-    }
-  },
-  computed: {
-  },
-  created() {
-  },
-  methods: {
-  },
 
 }
 </script>
 
 <template>
   <main-layout
-    v-if="$route.name === 'roles'"
     title="Roles"
   >
-    <table-layout
-      :fragments="false"
-      :hider="false"
-      :exporter="false"
-      table-name="RolesList"
-      :processor="processor"
-      :columns="columns"
-      :filters="filters"
-    >
-      <el-button
-        slot="actions"
-        icon="el-icon-plus"
-        circle
-      />
-    </table-layout>
+    <div>Roles placeholder</div>
   </main-layout>
-  <table-layout
-    v-else
-    :fragments="false"
-    :hider="false"
-    :exporter="false"
-    table-name="RolesList"
-    :processor="processor"
-    :columns="columns"
-    :filters="filters"
-  >
-    <el-button
-      slot="actions"
-      icon="el-icon-plus"
-      circle
-    />
-  </table-layout>
 </template>
--- packages/super/src/views/users/users.vue
+++ packages/super/src/views/users/users.vue
@@ -1,54 +1,10 @@
 <script>
-import Auth0ApiProcessor from '@lib/processors/temp/auth0-api-processor'
-import tableConfig from './users-table'
-
-const { VUE_APP_AUTH0_DOMAIN } = process.env
-
 export default {
-  name: 'UserManagement',
-  page: {
-    title: 'User management',
-  },
-  data() {
-    return {
-      processor: new Auth0ApiProcessor({
-        disableQueryString: true,
-        query: {
-          hide: [
-            'user_id',
-          ],
-        },
-        component: this,
-        path: `https://${VUE_APP_AUTH0_DOMAIN}/api/v2/users`,
-      }),
-      filters: tableConfig.filters,
-      columns: tableConfig.columns,
-    }
-  },
-  methods: {
-    onRowClick(row) {
-      this.$router.push({
-        name: 'user',
-        params: {
-          id: row.user_id || 'unknown',
-        },
-      })
-    },
-  },
 }
 </script>
 
 <template>
   <main-layout title="Users">
-    <table-layout
-      :fragments="false"
-      :hider="false"
-      :exporter="false"
-      table-name="userList"
-      :processor="processor"
-      :columns="columns"
-      :filters="filters"
-      @row-click="onRowClick"
-    />
+    <div>Users placeholder</div>
   </main-layout>
 </template>
