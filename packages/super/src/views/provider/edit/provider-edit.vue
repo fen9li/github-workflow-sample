e3522cef404b40f8b90fb39a269c0f1a00c6e0e0
Anton Simonov <me@16051976.ru>
2019-09-03 17:22:01 +0300

    feat(super): add provider details page and mocks

--- packages/super/src/views/provider/edit/provider-edit.vue
+++ packages/super/src/views/provider/edit/provider-edit.vue
@@ -47,6 +47,7 @@ export default {
         ],
         products: [
           {
+            type: 'array',
             required: true,
             message: 'This field is required',
             trigger: 'change',
@@ -77,72 +78,147 @@ export default {
 </script>
 
 <template>
-  <el-form
-    ref="form"
-    :model="form"
-    :rules="rules"
-    label-width="180px"
-    label-position="top"
-  >
-    <el-form-item
-      label="Provider Name"
-      prop="name"
+  <el-card>
+    <el-form
+      ref="form"
+      :model="form"
+      :rules="rules"
+      :class="$style.form"
+      label-position="top"
     >
-      <el-input
-        v-model="form.name"
-        placeholder="Name"
-      />
-    </el-form-item>
-    <el-form-item
-      label="Provider Description"
-      prop="description"
-      type="textarea"
-    >
-      <el-input
-        v-model="form.description"
-        placeholder="Description"
-      />
-    </el-form-item>
-    <el-form-item
-      label="Provider Timezone"
-      prop="timezone"
-    >
-      <el-select
-        v-model="form.timezone"
-        placeholder="please select timezone"
-        disabled
+      <el-form-item
+        label="Provider Name"
+        prop="name"
       >
-        <el-option
-          :label="form.timezone"
-          :value="form.timezone"
+        <el-input
+          v-model="form.name"
+          placeholder="Name"
         />
-      </el-select>
-    </el-form-item>
-    <el-form-item
-      prop="2fa"
-    >
-      <el-checkbox
-        v-model="provider['2fa']"
-        label="Enforce Two-Factor Authen6ca6on"
-      />
-    </el-form-item>
-    <el-form-item
-      label="Status"
-      prop="status"
-    >
-      <el-select
-        v-model="form.status"
-        placeholder="Please select status"
+      </el-form-item>
+      <el-form-item
+        label="Provider Description"
+        prop="description"
+        type="textarea"
       >
-        <el-option
-          label="Active"
-          value="active"
+        <el-input
+          v-model="form.description"
+          placeholder="Description"
         />
-        <el-option
-          label="Inactive"
-          value="inactive"
+      </el-form-item>
+      <el-form-item
+        label="Provider Timezone"
+        prop="timezone"
+      >
+        <el-select
+          v-model="form.timezone"
+          placeholder="please select timezone"
+          disabled
+        >
+          <el-option
+            :label="form.timezone"
+            :value="form.timezone"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item
+        prop="2fa"
+      >
+        <el-checkbox
+          v-model="provider['2fa']"
+          label="Enforce Two-Factor Authen6ca6on"
         />
-      </el-select>
-    </el-form-item>
-  </el-form>
+      </el-form-item>
+      <el-form-item
+        label="Status"
+        prop="status"
+      >
+        <el-select
+          v-model="form.status"
+          placeholder="Please select status"
+        >
+          <el-option
+            label="Active"
+            value="active"
+          />
+          <el-option
+            label="Inactive"
+            value="inactive"
+          />
+        </el-select>
+      </el-form-item>
+      <!-- TODO Versions -->
+      <el-form-item
+        label="Products"
+        prop="products"
+        :class="$style.products"
+      >
+        <div
+          v-for="(product, index) in form.products"
+          :key="index"
+          :class="$style.product"
+        >
+          <el-checkbox
+            :key="index"
+            :label="product.name"
+            :class="$style.productCheckbox"
+            disabled
+          />
+          <el-select
+            :key="`select_${index}`"
+            v-model="product.defaultVersion"
+            :class="$style.productSelect"
+            placeholder="please select version"
+          >
+            <el-option
+              v-for="(version, i) in product.versions"
+              :key="i"
+              :value="version"
+            />
+          </el-select>
+        </div>
+      </el-form-item>
+      <!-- /TODO Versions -->
+      <div :class="$style.formFooter">
+        <el-button
+          type="primary"
+          plain
+        >
+          Cancel
+        </el-button>
+        <el-button
+          type="primary"
+        >
+          Save
+        </el-button>
+      </div>
+    </el-form>
+  </el-card>
 </template>
+
+<style lang="scss" module>
+.form {
+  max-width: rem(470px);
+}
+
+.products {
+  display: flex;
+  flex-direction: column;
+
+  :global {
+    .el-form-item__content {
+      flex-direction: column;
+    }
+  }
+}
+
+.product {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  margin: rem(0 0 20px);
+}
+
+.productCheckbox {
+  width: rem(200px);
+}
+</style>
