17261b508694c6665749fbd6c6e92329d2e91ccc
midrock <iteam4u@ya.ru>
2019-09-04 00:01:20 +0300

    feat(super): add api processor to products

--- packages/super/src/views/products/products.table.js
+++ packages/super/src/views/products/products.table.js
@@ -1,5 +1,5 @@
-// import ApiProcessor from '@lib/processors/api-processor'
-import MockProcessor from '@lib/processors/mock-processor'
+import ApiProcessor from '@lib/processors/api-processor'
+// import MockProcessor from '@lib/processors/mock-processor'
 
 const TABLE_FILTERS = [
   {
@@ -110,9 +110,9 @@ const TABLE_COLUMNS = [
 
 export default function(component) {
   return {
-    processor: new MockProcessor({
+    processor: new ApiProcessor({
       component,
-      mockFrom: 'products',
+      path: 'products',
     }),
     filters: TABLE_FILTERS,
     columns: TABLE_COLUMNS,
