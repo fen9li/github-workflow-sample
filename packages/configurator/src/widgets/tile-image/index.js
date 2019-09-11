b4fa8c51a937d121e47f6e52c96b332ad5a89186
midrock <iteam4u@ya.ru>
2019-09-11 23:48:24 +0300

    feat(configurator): create builder for tile-image

---
+++ packages/configurator/src/widgets/tile-image/index.js
@@ -0,0 +1,9 @@
+import TileImagePreview from './tile-image-preview'
+import TileImageBuilder from './tile-image-builder'
+
+export default {
+  name: 'tile-image',
+  title: 'Tile with Image',
+  preview: TileImagePreview,
+  builder: TileImageBuilder,
+}
---
+++ packages/configurator/src/widgets/tile-image/tile-image-builder.vue
@@ -0,0 +1,67 @@
+<script>
+import BuilderMixin from '../builder.mixin'
+
+export default {
+  name: 'TileImageBuilder',
+  mixins: [BuilderMixin],
+  data: () => ({
+    form: null,
+  }),
+  created() {
+    if (!this.form) {
+      this.form = {
+        align: 'left',
+        imageSize: 'half',
+        space: true,
+        ...this.initialConfig.props,
+      }
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="hero-builder">
+    <builder-textarea
+      v-model="form.title"
+      label="Title"
+    />
+    <builder-textarea
+      v-model="form.text"
+      label="Text"
+    />
+    <builder-image
+      v-model="form.image"
+      :ratio="{
+        mobile: '13:4',
+        desktop: '32:5',
+      }"
+      label="Image"
+    />
+    <builder-cta
+      v-model="form.cta"
+      label="Link"
+    />
+    <builder-radio
+      v-model="form.align"
+      label="Text tile alignment"
+      :options="[
+        { label: 'Left', value: 'left' },
+        { label: 'Right', value: 'right' },
+      ]"
+    />
+    <builder-radio
+      v-model="form.imageSize"
+      label="Image Size"
+      :options="[
+        { label: '1/2', value: 'half' },
+        { label: '3/4', value: 'long' },
+      ]"
+    />
+    <builder-radio
+      v-model="form.space"
+      label="Space between"
+      type="boolean"
+    />
+  </div>
+</template>
