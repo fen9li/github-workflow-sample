9ee655a1a487e1b8511c69af4a166793f1d82367
midrock <iteam4u@ya.ru>
2019-09-12 22:39:58 +0300

    feat(configurator): add base-block-editable component

---
+++ packages/configurator/src/components/base/base-block-editable.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'BaseBlockEditable',
+}
+</script>
+
+<template>
+  <base-block
+    class="base-block-editable"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <template slot="actions">
+      <el-button
+        icon="el-icon-arrow-down"
+        @click.stop.prevent="$emit('down')"
+      />
+      <el-button
+        icon="el-icon-arrow-up"
+        @click.stop.prevent="$emit('up')"
+      />
+      <el-button
+        icon="el-icon-delete"
+        @click.stop.prevent="$emit('delete')"
+      />
+    </template>
+    <slot />
+  </base-block>
+</template>
