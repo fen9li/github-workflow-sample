38c1a57eebf365a59381b1521d50f2d8f68b94a2
Konstantin Zakablukovsky <2cynicus@gmail.com>
2019-09-20 13:24:19 +1000

    feat(es-supplier-products): create layout

--- packages/estore/src/views/supplier-products/supplier-products.vue
+++ packages/estore/src/views/supplier-products/supplier-products.vue
@@ -1,11 +1,44 @@
 <script>
+import supplierProductsTable from './supplier-products.table'
+import getExportedFilename from '@lib/utils/get-exported-filename'
+
 export default {
   name: 'SupplierProducts',
+  page: {
+    title: 'Supplier Products',
+  },
+  data() {
+    return {
+      table: supplierProductsTable(this),
+    }
+  },
+  methods: {
+    onRowClick(row) {
+      this.$router.push({
+        name: 'supplier-product',
+        params: {
+          id: row.id,
+        },
+      })
+    },
+    getExportedFilename() {
+      return getExportedFilename(this.table.tableName)
+    },
+  },
 }
 </script>
 
 <template>
-  <div>
-    Supplier Products view
-  </div>
+  <main-layout title="Supplier Products">
+    <table-layout
+      :table-name="table.tableName"
+      :fragments="false"
+      hider
+      :processor="table.processor"
+      :filters="table.filters"
+      :columns="table.columns"
+      :export-filename="getExportedFilename"
+      @row-click="onRowClick"
+    />
+  </main-layout>
 </template>
---
+++ packages/estore/tests/__fixtures__/supplier-products.js
@@ -0,0 +1,110 @@
+module.exports = {
+  table: [
+    {
+      "created_at": "1973-12-15T19:36:49Z",
+      "id": "473958b6-d80e-11e9-afe8-0242ac120003",
+      "updated_at": "1984-07-06T04:02:08Z",
+      "availability_data": {
+        "enabled": true,
+        "status": "Available"
+      },
+      "brand": "et",
+      "category_data": [],
+      "currency": "AUD",
+      "description": "Id suscipit nesciunt cumque eveniet asperiores.",
+      "dimensions_data": {
+        "unit": "in",
+        "height": 2,
+        "length": 8,
+        "depth": 5
+      },
+      "gtin": "ducimus",
+      "images": [
+        "https://lorempixel.com/640/480/?38400"
+      ],
+      "name": "rerum",
+      "model": "cum",
+      "mpn": "qui",
+      "pricing_data": {
+        "costpriceBeforeTax": 4.96,
+        "priceBeforeTax": 1131074.59,
+        "priceAfterTax": 2036002.58,
+        "rrpBeforeTax": 101625.3,
+        "rrpAfterTax": 26.01
+      },
+      "shipping_cost_data": {
+        "shippingBeforeTax": 7994153.91,
+        "shippingAfterTax": 5.85
+      },
+      "shipping_dimensions": {
+        "unit": "in",
+        "height": 2,
+        "length": 0,
+        "depth": 9
+      },
+      "shipping_weight_data": {
+        "unit": "lb",
+        "value": 9
+      },
+      "sku": "quos",
+      "supplier_id": "470cb97e-d80e-11e9-afe8-0242ac120003",
+      "weight_data": {
+        "unit": "lb",
+        "value": 8
+      }
+    },
+    {
+      "created_at": "2014-01-27T03:39:42Z",
+      "id": "474e6efd-d80e-11e9-afe8-0242ac120003",
+      "updated_at": "1988-06-20T13:35:53Z",
+      "availability_data": {
+        "enabled": true,
+        "status": "Available"
+      },
+      "brand": "exercitationem",
+      "category_data": [],
+      "currency": "AUD",
+      "description": "Consectetur aliquam voluptatem molestiae consequuntur eum delectus soluta.",
+      "dimensions_data": {
+        "unit": "in",
+        "height": 5,
+        "length": 8,
+        "depth": 8
+      },
+      "gtin": "quia",
+      "images": [
+        "https://lorempixel.com/640/480/?57205"
+      ],
+      "name": "aspernatur",
+      "model": "sint",
+      "mpn": "laborum",
+      "pricing_data": {
+        "costpriceBeforeTax": 4433331.06,
+        "priceBeforeTax": 253732.89,
+        "priceAfterTax": 651760.35,
+        "rrpBeforeTax": 83.34,
+        "rrpAfterTax": 409469563.12
+      },
+      "shipping_cost_data": {
+        "shippingBeforeTax": 13632.49,
+        "shippingAfterTax": 47.69
+      },
+      "shipping_dimensions": {
+        "unit": "in",
+        "height": 7,
+        "length": 9,
+        "depth": 7
+      },
+      "shipping_weight_data": {
+        "unit": "lb",
+        "value": 5
+      },
+      "sku": "delectus",
+      "supplier_id": "470cb97e-d80e-11e9-afe8-0242ac120003",
+      "weight_data": {
+        "unit": "lb",
+        "value": 7
+      }
+    }
+  ]
+}
